2d55a36853314024c1cc414a3673b30113698ddf
Henrique Cardoso <132698065+Henrique-1961@users.noreply.github.com>
2023-08-03 10:56:02 +0000

    Adicionado - Conexão com BD

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-import express from "express";  
+import express from "express";
+import dotenv from "dotenv";
+import { selectUsuarios } from "./bd.js";
+
 const app = express();              
-const port = 3000;             
+const port = 3000;  
+dotenv.config();            
 
 app.get("/", (req, res) => {        
     res.json({
         nome: "Henrique Cardoso de Souza",     
     });
     console.log("Rota / solicitada");
+});
+
+app.get("/usuarios", async (req, res) => {
+    console.log("Rota GET/usuarios solicitada");
+    try {
+        const usuarios = await selectUsuarios();
+        res.json(usuarios);
+    } catch (error) {
+        res.status(error.status || 500).json({ message: error.message || "Erro!" });
+    }
 });
 
 app.listen(port, () => {
