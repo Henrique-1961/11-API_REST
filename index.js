e4abc70939a37f1e48f6f388db7bd6273e8e4534
Henrique Cardoso <132698065+Henrique-1961@users.noreply.github.com>
2023-08-04 01:17:52 +0000

    Atualizado - Atividade 13

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
-import { selectUsuarios } from "./bd.js";
+import { selectUsuarios, insertUsuario, deleteUsuario } from "./bd.js";
 
 dotenv.config();  
 
