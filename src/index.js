76b02213d06e12e398afa4b3b63de7bfae2b6641
Henrique Cardoso <132698065+Henrique-1961@users.noreply.github.com>
2023-08-24 11:54:48 +0000

    Atividade 15 - Teste 01

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ const app = express();
 const port = 3000;         
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(roteadorUsuario);
 app.use(loginRouter);
-app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {        
     res.json({
