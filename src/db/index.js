149bda80f582538952c46740928f0f30ad064980
Henrique Cardoso <132698065+Henrique-1961@users.noreply.github.com>
2023-08-24 11:48:24 +0000

    Adicionado - Atividade 15

--- src/db/index.js
+++ src/db/index.js
@@ -8,6 +8,14 @@ async function connect() {
   return pool.connect();
 }
 
+async function autenticarUsuario(email, senha) {
+  const client = await connect();
+  const query = "SELECT * FROM usuario WHERE email = $1 AND senha = $2";
+  const usuario = [email, senha];
+  const res = await client.query(query, usuario);
+  return res.rows[0];
+}
+
 async function selectUsuarios() {
   const client = await connect();
   const res = await client.query("SELECT * FROM usuario");
@@ -43,4 +51,4 @@ async function updateUsuario(data) {
   await client.query(query, usuario);
 }
 
-export { selectUsuarios, insertUsuario, deleteUsuario, selectUsuario, updateUsuario };
+export { selectUsuarios, insertUsuario, deleteUsuario, selectUsuario, updateUsuario, autenticarUsuario };
--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ dotenv.config();
 
 import express from "express";
 import roteadorUsuario from "./routes/usuario.js";
-
+import loginRouter from "./routes/login.js";
 
 const app = express();              
 const port = 3000;         
 
 app.use(express.json());
 app.use(roteadorUsuario);
+app.use(loginRouter);
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {        
     res.json({
